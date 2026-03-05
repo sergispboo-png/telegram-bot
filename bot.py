21750ade099c23ddb14f8ec208d640c1cc174b9c
sergispboo-png <sergispboo@gmail.com>
2026-03-05 21:32:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -464,7 +464,20 @@ async def process_prompt(message: Message, state: FSMContext):
         ERROR_LOG.append(str(e))
         await status.edit_text("❌ Ошибка сервера.", reply_markup=after_generation_menu())
 
+@dp.callback_query(F.data == "user_agreement")
+async def user_agreement(callback: CallbackQuery):
 
+    file = BufferedInputFile(
+        open("terms.html", "rb").read(),
+        filename="LuxRender_User_Agreement.html"
+    )
+
+    await callback.message.answer_document(
+        file,
+        caption="📄 Пользовательское соглашение LuxRender"
+    )
+
+    await callback.answer()
 # ================= АДМИН =================
 
 @dp.message(Command("stats"))
